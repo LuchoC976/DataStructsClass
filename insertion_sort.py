bb0c57bc66df889b15e7917ab48dbd490809c9db
Luis Cagigal <luis@iPhone-Cris.usfq.edu.ec>
2021-09-01 11:12:06 -0500

    commented insertion sort python

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,11 +5,17 @@ my_arr = [8, 5, 4, 10, 9]
 def insertionSort(arr):
     
     for i in range(1,len(arr)):
+        # Setting position marker for sorted array
         curr = arr[i]
         prev_pos = i - 1
+
+        # Organize unsorted array into sorted one
         while prev_pos >= 0 and curr < arr[prev_pos]:
+            # Switch unsorted places
             arr[prev_pos+1] = arr[prev_pos]
             prev_pos -= 1
+
+        # Update marker
         arr[prev_pos+1] = curr
     
     return arr
