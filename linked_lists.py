38d4906244e052f42d79de26155e9362fd8c9398
Luis Cagigal <luis@Luiss-MacBook-Pro.local>
2021-09-09 10:36:17 -0500

    linked list with insert() implementation

--- linked_lists.py
+++ linked_lists.py
@@ -21,12 +21,24 @@ class SingleLinkedList:
             node = node.next
 
     # Insert implementation
-    '''
-    def insert(self, list, index, node):
-        if index == 0:
-            node.set_next()
-            return SingleLinkedList(node)    
-    '''
+    
+    def insert(self, add_node, prev_node=None):
+        # Adding to start
+        if prev_node == None:
+            add_node.set_next(self.head)
+            self.head = add_node
+        else:
+            # Adding to end
+            if prev_node.next == None:
+                prev_node.next = add_node
+            else:
+                # Adding in between nodes
+                temp = prev_node.next
+                prev_node.next = add_node
+                add_node.next = temp
+            
+
+
 # Linked list example
 # [Jan] -> [Feb] -> [March] -> [None]
 m1 = Node("Jan")
@@ -37,5 +49,27 @@ m1.set_next(m2)
 m2.set_next(m3)
 
 list1 = SingleLinkedList(m1)
+
+list1.printList()
+
+print("Inserting node at start (December node)")
+m4 = Node("Dec")
+
+
+list1.insert(m4)
+
+list1.printList()
+
+print("Inserting node at end (May node)")
+
+m5 = Node("May")
+list1.insert(m5,m3)
+
+list1.printList()
+
+print("Inserting in between nodes (April node)")
+
+m6 = Node("April")
+list1.insert(m6,m3)
 
 list1.printList()
