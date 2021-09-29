6b94505df3087c285af02e133bce672b49d38c5c
Luis Cagigal <luis@Luiss-MacBook-Pro.local>
2021-09-29 10:19:50 -0500

    recursion fibonacci

--- recursion.py
+++ recursion.py
@@ -6,12 +6,15 @@ def fib(n):
     cache = {}
 
     def recursive_fib(n):
+        # Memoization
         if n in cache.keys():
             return cache[n]
         
+        # Base case
         if n == 0 or n == 1:
             res = n
         else:
+            # Recursive relationship
             res = recursive_fib(n-1) + recursive_fib(n-2)
         
         cache[n] = res
